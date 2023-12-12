c588fc6dcc2ccf844cbda22560ff85b64a460415
Izsal Qurlinas Afandi <izsalavandi@gmail.com>
2023-12-13 05:36:06 +0700

    feat: 🎸 First Steps Toward Out Tic-Tac-Toc Game

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,20 @@ function App() {
 
 
   return (
-    <p>Coming Soon...</p>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li><span className="player-name">Players 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li><span className="player-name">Players 2</span>
+            <span className="player-symbol">O</span></li>
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
+  );
 }
 
 export default App
